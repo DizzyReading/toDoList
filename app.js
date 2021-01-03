14025ee5908d264190b2e01c37f855daf8a5449e
DizzyReading <daintybeat@gmail.com>
2021-01-03 12:28:06 +0530

    step 5(imp)

--- app.js
+++ app.js
@@ -3,65 +3,44 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({
+  extended: true 
+}));
 
 app.get("/", function(req, res) {
 
   const today = new Date();
-  const currentDay = today.getDay();
-  var day = "";
 
-  // if (currentDay === 6 || currentDay === 0) {
-  //   day = "Weekend";
-  // } else {
-  //   day = "Weekday";
-  // }
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
 
-  switch (currentDay) {
-
-    case 0:
-      day = "Sunday";
-      break;
-
-    case 1:
-      day = "Monday";
-
-      break;
-
-    case 2:
-      day = "Tuesday";
-
-      break;
-
-    case 3:
-      day = "Wednesday";
-
-      break;
-
-    case 4:
-      day = "Thursday";
-      break;
-
-    case 5:
-      day = "Friday";
-
-      break;
-
-    case 6:
-      day = "Saturday";
-
-      break;
-
-    default:
-    console.log("Error: current day is equal to: " + currentDay);
-
-  }
+  var day = today.toLocaleDateString("en-US", options);
 
   res.render("list", {
-    kindOfDay: day
+    kindOfDay: day,
+    newListItems: items,
   });
 
 });
+
+app.post("/", function(req, res) {
+
+  var item = req.body.newTask
+
+  items.push(item);
+
+  res.redirect("/");
+
+  console.log(item);
+
+})
 
 app.listen("3000", function() {
   console.log("Server is running on port 3000 . . .");
